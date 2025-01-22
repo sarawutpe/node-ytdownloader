d0b3adc58e03863d692cc1efd312aa8325771bb4
Sarawut Pe <mr.sarawutpe@gmail.com>
2025-01-23 00:54:13 +0700

    fix metadata

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(express.json());
 
 const middleware = (req, res, next) => {
@@ -43,12 +43,23 @@ app.get("/ping", (req, res) => {
 });
 
 app.post("/metadata", middleware, async (req, res) => {
-  const { url } = req.body;
-  const params = new URL(url).searchParams;
-  const v = params.get("v");
-  const newUrl = `https://www.youtube.com/watch?v=${v}`;
-  const info = await ytdl.getInfo(newUrl);
-  res.status(200).json({ status: true, data: info.videoDetails });
+  try {
+    const { url } = req.body;
+    const params = new URL(url).searchParams;
+    const v = params.get("v");
+    const newUrl = `https://www.youtube.com/watch?v=${v}`;
+    const cookies = req.headers.cookie;
+    if (!cookies) {
+      return res.status(400).json({ status: false, error: "Cookies are required for authentication." });
+    }
+    const agentOptions = {};
+    const agent = ytdl.createAgent(cookies, agentOptions);
+    const info = await ytdl.getBasicInfo(newUrl, { agent });
+    res.status(200).json({ status: true, data: info.videoDetails });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, error: "Failed to fetch video metadata" });
+  }
 });
 
 app.post("/download", middleware, async (req, res) => {
